78f8598f6271312ff86a005cfd0fdd9fb4424e57
Hairun Huang <hairunhuang@gmail.com>
2024-06-03 15:52:32 -0700

    gallery

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Banner from "@/components/Banner";
 import DemoVideo from "@/components/DemoVideo";
+import Gallery from "@/components/Gallery";
 import Introduction from "@/components/Introduction";
 
 export default async function Home() {
@@ -9,6 +10,12 @@ export default async function Home() {
       <Banner />
       <Introduction />
       <DemoVideo />
+      <div className="h-16 bg-white" />
+      <div className="py-6 bg-[#EEE] text-[#FF6600] text-center text-[1.9rem]">
+        <h3>Like us so far?</h3>
+        <h3>Check out our work</h3>
+      </div>
+      <Gallery />
     </>
   );
 }
---
+++ src/components/Gallery.js
@@ -0,0 +1,46 @@
+import { Search } from 'lucide-react';
+import React from 'react'
+
+function Gallery() {
+    const projects = [
+        {
+            title: "macs-circlek-flip-lip",
+            imgUrl: "/FTLF4-thumb-550x550.jpg",
+        },
+        {
+            title: "stride-crunch",
+            imgUrl: "/stride-feature-image-550x550.jpg"
+        },
+        {
+            title: "earths-choice-coffee",
+            imgUrl: "/ec-packaging-coffee-thumb-550x550.jpg",
+        },
+        {
+            title: "sunco-dried-fruit",
+            imgUrl: "/sunco-fruit-bags-thumb-550x550.jpg"
+        },
+        {
+            title: "aims-manitoba-home",
+            imgUrl: "/aims-manitoba-home-thumb-550x550.jpg"
+        },
+        {
+            title: "metrie-catalogue",
+            imgUrl: "/mm-covers-thumb2-550x550.jpg"
+        }
+    ];
+
+    return (
+        <div className="bg-[#EEE] grid grid-cols-3 gap-3">
+            {projects.map((project, index) => (
+                <div key={index} className="relative overflow-hidden group">
+                    <img src={project.imgUrl} alt={project.title} className="w-full h-auto transform transition-transform duration-300 group-hover:scale-110" />
+                    <div className="absolute inset-0 bg-black bg-opacity-75 flex justify-center items-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                        <Search className="w-7 h-7 text-white font-bold" />
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Gallery
